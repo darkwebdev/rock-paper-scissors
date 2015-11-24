8ccf20baaf33cc6892ac0d82189285a6af33a7a0
tibalt <gtibalt@gmail.com>
2015-11-24 10:55:26 +0100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const expect = require('chai').expect;
+const doesFirstWin = {
+    rock: {
+        paper: false,
+        scissors: true
+    },
+    paper: {
+        rock: true,
+        scissors: false
+    },
+    scissors: {
+        rock: false,
+        paper: true
+    }
+};
+
+describe('Rock-paper-scissors', () => {
+    context('Rock vs Paper', () => {
+        it('Paper wins', () => {
+            expect(doesFirstWin.rock.paper).to.be.false;
+            expect(doesFirstWin.paper.rock).to.be.true;
+        });
+    });
+    context('Rock vs Scissors', () => {
+        it('Rocks wins', () => {
+            expect(doesFirstWin.rock.scissors).to.be.true;
+            expect(doesFirstWin.scissors.rock).to.be.false;
+        });
+    });
+
+    context('Paper vs Scissors', () => {
+        it('Rocks wins', () => {
+            expect(doesFirstWin.paper.scissors).to.be.false;
+            expect(doesFirstWin.scissors.paper).to.be.true;
+        });
+    });
+});
