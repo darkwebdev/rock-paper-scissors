03bf729e377b9408d7e765ef4e15bf5bfe4217b4
tibalt <gtibalt@gmail.com>
2015-11-24 11:38:07 +0100

    random generator tests added

--- test/test.js
+++ test/test.js
@@ -15,25 +15,46 @@ const doesFirstWin = {
         paper: true
     }
 };
+const things = Object.keys(doesFirstWin);
 
 describe('Rock-paper-scissors', () => {
-    context('Rock vs Paper', () => {
-        it('Paper wins', () => {
-            expect(doesFirstWin.rock.paper).to.be.false;
-            expect(doesFirstWin.paper.rock).to.be.true;
+    describe('game logic', () => {
+        context('Rock vs Paper', () => {
+            it('Paper wins', () => {
+                expect(doesFirstWin.rock.paper).to.be.false;
+                expect(doesFirstWin.paper.rock).to.be.true;
+            });
+        });
+        context('Rock vs Scissors', () => {
+            it('Rock wins', () => {
+                expect(doesFirstWin.rock.scissors).to.be.true;
+                expect(doesFirstWin.scissors.rock).to.be.false;
+            });
+        });
+        context('Paper vs Scissors', () => {
+            it('Scissors win', () => {
+                expect(doesFirstWin.paper.scissors).to.be.false;
+                expect(doesFirstWin.scissors.paper).to.be.true;
+            });
         });
     });
-    context('Rock vs Scissors', () => {
-        it('Rock wins', () => {
-            expect(doesFirstWin.rock.scissors).to.be.true;
-            expect(doesFirstWin.scissors.rock).to.be.false;
-        });
-    });
+    describe('things generator', () => {
+        things.forEach((thing, i) => {
+            it('should return ' + thing, () => {
+                const getRandomElem = (arr, randomFn) => arr[randomFn(arr.length)];
+                const fakeRandom = () => i;
 
-    context('Paper vs Scissors', () => {
-        it('Scissors win', () => {
-            expect(doesFirstWin.paper.scissors).to.be.false;
-            expect(doesFirstWin.scissors.paper).to.be.true;
+                expect(things.indexOf(getRandomElem(things, fakeRandom))).to.equal(i);
+            });
+        });
+    });
+    describe('random number generator', () => {
+        it('should return integers within range', () => {
+            const getRandomIntExcluding = max => Math.round(Math.random() * max);
+
+            for (let i=0; i<100; i++) {
+                expect(getRandomIntExcluding(3)).to.be.within(0, 3);
+            }
         });
     });
 });
